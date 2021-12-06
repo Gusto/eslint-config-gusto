d87925d09010ec03594cdb786a2e7a71216a211e
Mark Wheeler <mhwheeler@gmail.com>
2021-12-06 12:25:15 -0800

    use the config directly

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['airbnb', 'plugin:prettier/recommended', 'plugin:react-hooks/recommended'],
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
   parser: 'babel-eslint',
   env: {
     es6: true,
